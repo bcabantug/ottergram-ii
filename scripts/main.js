f510f29a9433fd6b4399fbdb2641cf8dd506dbc2
Brian <ballerbrian4@gmail.com>
2018-02-25 12:33:03 -0800

    added functionality for buttons to cycle images

--- scripts/main.js
+++ scripts/main.js
@@ -44,8 +44,74 @@ function getThumbnailsArray(){
 function initializeEvents(){
   "use strict";
   var thumbnails = getThumbnailsArray(); //gets the thumbnail array
-  thumbnails.forEach(addThumbClickHandler); //adds event handler to each thumbnail\
+  thumbnails.forEach(addThumbClickHandler); //adds event handler to each thumbnail\; lack of () adds it as "property" of that function
 
+  //test for left button changeImage
+  leftButtonCycle();
+  //
+  rightButtonCycle();
+}
+
+//functions to have buttons cycle through the pictures
+function leftButtonCycle(){
+  "use strict";
+  var left = document.querySelector(".left-button"); //selects the left button
+
+  left.addEventListener("click", function(){
+    //var currImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    var currTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+
+    var thumbList = getThumbnailsArray();
+
+    for(var i = 0; i < thumbList.length; i++){
+      if(thumbList[i].getAttribute("data-image-title") == currTitle.textContent){
+        if(i == 0){
+          var changeImage = imageFromThumb(thumbList[thumbList.length-1]);
+          var changeTitle = titleFromThumb(thumbList[thumbList.length-1]);
+
+          setDetails(changeImage, changeTitle);
+          break;
+        }
+
+        else if(i != 0){
+          changeImage = imageFromThumb(thumbList[i-1]);
+          changeTitle = titleFromThumb(thumbList[i-1]);
+
+          setDetails(changeImage, changeTitle);
+        }
+      }
+    }
+  });
+}
+function rightButtonCycle(){
+  "use strict";
+  var right = document.querySelector(".right-button"); //selects the left button
+
+  right.addEventListener("click", function(){
+    //var currImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    var currTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+
+    var thumbList = getThumbnailsArray();
+
+    for(var i = 0; i < thumbList.length; i++){
+      if(thumbList[i].getAttribute("data-image-title") == currTitle.textContent){
+        if(i == thumbList.length-1){
+          var changeImage = imageFromThumb(thumbList[0]);
+          var changeTitle = titleFromThumb(thumbList[0]);
+
+          setDetails(changeImage, changeTitle);
+        }
+
+        else{
+          changeImage = imageFromThumb(thumbList[i+1]);
+          changeTitle = titleFromThumb(thumbList[i+1]);
+
+          setDetails(changeImage, changeTitle);
+          break;
+        }
+      }
+    }
+  });
 }
 
 initializeEvents();
